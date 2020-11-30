0586a09a72fddb35f30bada6256e8a4573e41aec
K Annabel Bligh <kabligh@gmail.com>
2020-11-30 16:54:48 +0000

    Refactored and added game methods

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -2,21 +2,22 @@ describe('Fizzbuzz', function() {
 
   var fizzbuzz;
 
+  beforeEach(function() {
+    fizzbuzz = new Fizzbuzz();
+  });
+
   describe('knows when a number is', function() {
 
     it('divisible by 3', function() {
-      fizzbuzz = new Fizzbuzz();
       expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
     });
 
     it('divisible by 5', function() {
-      fizzbuzz = new Fizzbuzz();
       expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
     });
 
-    it('divisible by 3 and 5', function() {
-      fizzbuzz = new Fizzbuzz();
-      expect(fizzbuzz.isDivisibleByThreeAndFive(15)).toBe(true);
+    it('divisible by 15', function() {
+      expect(fizzbuzz.isDivisibleByFifteen(15)).toBe(true);
     });
 
   });
@@ -24,18 +25,35 @@ describe('Fizzbuzz', function() {
   describe('knows when a number is NOT', function() {
 
     it('divisible by 3', function() {
-      fizzbuzz = new Fizzbuzz();
       expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
     });
 
     it('divisible by 5', function() {
-      fizzbuzz = new Fizzbuzz();
       expect(fizzbuzz.isDivisibleByFive(2)).toBe(false);
     });
 
-    it('divisible by 3 and 5', function() {
-      fizzbuzz = new Fizzbuzz();
-      expect(fizzbuzz.isDivisibleByThreeAndFive(3)).toBe(false);
+    it('divisible by 15', function() {
+      expect(fizzbuzz.isDivisibleByFifteen(3)).toBe(false);
+    });
+
+  });
+
+  describe('when playing, says', function() {
+
+    it('"Fizz" when a number is divisible by 3', function() {
+      expect(fizzbuzz.says(3)).toEqual("Fizz");
+    });
+
+    it('"Buzz" when a number is divisible by 5', function() {
+      expect(fizzbuzz.says(5)).toEqual("Buzz");
+    });
+
+    it('"Fizzbuzz" when a number is divisible by 15', function() {
+      expect(fizzbuzz.says(15)).toEqual("Fizzbuzz");
+    });
+
+    it('the number when it is not divisble by 3, 5 or 15', function() {
+      expect(fizzbuzz.says(2)).toEqual(2);
     });
 
   });
